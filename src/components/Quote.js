941019095fcebea360878ad1586b82193ed12948
oyelakinG9 <oyelakinr37@gmail.com>
2022-02-15 22:49:37 +0100

    Add quote file

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Quote.css';
+
+const Quote = () => (
+  <div className="quote">
+    <p>
+      Mathematics is not about number,equation,scompulation or algorithm:It is about understanading-
+    </p>
+    <i className="bold"> William Paul Thurston</i>
+  </div>
+);
+
+export default Quote;
