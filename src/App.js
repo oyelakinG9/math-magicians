0bfd17c02a5765f0b304a6926a48e10e738e4951
oyelakinG9 <oyelakinr37@gmail.com>
2022-02-15 22:54:45 +0100

    Create app router in app.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
 import Calculator from './components/calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +13,29 @@ class App extends Component {
 
   render() {
     return (
-      <Calculator />
+      <div className="App">
+        <header className="header">
+          <h1>Math Magicians</h1>
+          <nav className="nav">
+            <ul className="navBar">
+              <li className="nav-link">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-link border">
+                <Link to="calculator">Calculator</Link>
+              </li>
+              <li className="nav-link">
+                <Link to="quote">Quote</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+        </Routes>
+      </div>
     );
   }
 }
