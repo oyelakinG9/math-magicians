7a8d2f45439616267240097809d857f906eca244
marysol <marilirulita@gmail.com>
2022-02-16 13:37:03 -0800

    Add test

--- src/tests/OPerate.test.js
+++ src/tests/OPerate.test.js
@@ -1,2 +1,20 @@
+import React, {useState} from 'react'
+import {render, fireEvent} from '@testing-library/react'
 import operate from "../logic/operate";
 import calculate from "../logic/calculate";
+
+test('adds 1 + 2 to equal 3', () => {
+  const obj = {
+    total: "1",
+    next: "2",
+    operation: "+",
+  }
+
+  const deleted = {
+    total: null,
+    next: null,
+    operation: null,
+  }
+  const buttonName = 'AC';
+  expect(calculate(obj, buttonName)).toBe(deleted);
+});
