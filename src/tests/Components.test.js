7931e01b5bf243a17ffff0ff84d9b980f12b8b89
marysol <marilirulita@gmail.com>
2022-02-17 09:02:37 -0800

    Add compnent test

--- src/tests/Components.test.js
+++ src/tests/Components.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {render, screen, cleanup, fireEvent,} from '@testing-library/react';
 import Calculator from '../components/calculator';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Calculator></Calculator>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+it('5 + 4', () => {
+  render(<Calculator />);
+
+  fireEvent.click(screen.getByText('5'));
+  fireEvent.click(screen.getByText('+'));
+  fireEvent.click(screen.getByText('4'));
+  fireEvent.click(screen.getByText('='));
+
+  expect(screen.getByText('9')).toMatchSnapshot();
 });
+
+afterEach(cleanup);
